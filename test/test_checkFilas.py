1a9b3b55f83c4aba716e61d5a5495c9d7884395f
ccastroto <castroaldrovercarlossangrinha@gmail.com>
2025-11-17 13:55:25 +0100

    test(checkFilas): Añadir lso casos test de checkFilas

---
+++ test/test_checkFilas.py
@@ -0,0 +1,23 @@
+from src.checkFilas import checkFilas
+import casosTest.casosTest as casosTest
+import pytest
+
+@pytest.mark.parametrize(
+    "sudoku, sano",
+    [
+        (casosTest.correcto, True),
+        (casosTest.numero_repetido_fila_columna, False),
+        (casosTest.numero_repetido_columna, True),
+        (casosTest.numero_no_presente, False),
+        (casosTest.numero_fuera_del_rango, False),
+        (casosTest.caracteres, False),
+        (casosTest.numeros_reales, False),
+        (casosTest.irregular_fila, True),
+        (casosTest.irregular_columna, False),
+        (casosTest.lista_vacia, False)
+    ],
+)
+
+def test_checkFilas(sudoku, sano):
+    assert checkFilas(sudoku) == sano
+    
